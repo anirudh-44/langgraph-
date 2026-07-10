50aad29d6fc33a620999f2a16a0217a345471343
Anirudh G <anirudhgadekar44@gmail.com>
2026-07-10 17:21:46 +0530

    chatbot observability

--- streamlit_frontend_chatbot_withDB.py
+++ streamlit_frontend_chatbot_withDB.py
@@ -36,7 +36,14 @@ if 'chat_threads' not in st.session_state:
 
 add_thread_id(st.session_state['thread_id'])
 
-CONFIG = {'configurable': {'thread_id': st.session_state['thread_id']}}
+#CONFIG = {'configurable': {'thread_id': st.session_state['thread_id']}}
+CONFIG = {
+        "configurable": {"thread_id": st.session_state["thread_id"]},
+        "metadata": {
+            "thread_id": st.session_state["thread_id"]
+        },
+        "run_name": "chat_turn",
+    }
 
 #*******************************SIDEBAR_UI*****************************************
 st.sidebar.title('LangGraph Chatbot')
